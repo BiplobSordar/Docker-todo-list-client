797e49414cca808f08b161482bea8159b7e3b410
BiplobSorder <biplob.code.bd@gmail.com>
2025-08-20 19:03:37 +0600

    ddws

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const API = process.env.REACT_APP_API_URL || 'http://backend:5000';
+const API = process.env.REACT_APP_API_URL || 'http://backend:5000/api';
 
 function App() {
   const [todos, setTodos] = useState([]);
