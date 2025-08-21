f91206487e23e043e74b6195292e0cbef4397ee2
BiplobSorder <biplob.code.bd@gmail.com>
2025-08-21 08:43:44 +0600

    this is the final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const API = process.env.REACT_APP_API_URL || 'http://backend:5000/api';
+const API = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
 function App() {
   const [todos, setTodos] = useState([]);
